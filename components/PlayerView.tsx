eb88176807c89d39bdab7da33886669c3a20dc4c
Benjamin Arko Afrasah <barkoafrasah@gmail.com>
2024-11-03 17:19:39 +0000

    feat: update adblocker

--- components/PlayerView.tsx
+++ components/PlayerView.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useWindowDimensions, View } from "react-native";
 import WebView from "react-native-webview";
 import { Colors } from "react-native/Libraries/NewAppScreen";
@@ -9,11 +10,25 @@ interface PlayerViewProps {
 
 const PlayerView = ({ link }: PlayerViewProps) => {
   const dimensions = useWindowDimensions();
+  const [mediaURL, setMediaURL] = React.useState<string | null>(null);
+
+  useEffect(() => {
+    setMediaURL(link);
+  }, []);
 
   const adBlockerJS = `
     document.querySelectorAll('[id*="ad"], [class*="ad"], [class*="popup"], [id*="popup"]').forEach(el => el.remove());
     true; // Required to prevent WebView from showing error
   `;
+
+  const searchParams = new URLSearchParams(mediaURL?.split("?")[1]);
+  searchParams.set("autoplay", "true");
+  searchParams.set("ds_lang", "en");
+  const finalUrl = mediaURL ? `${mediaURL.split("?")[0]}?${searchParams.toString()}` : null;
+
+  console.log("finalUrl", finalUrl);
+
+  if (!mediaURL) return null;
 
   return (
     <View
@@ -23,10 +38,11 @@ const PlayerView = ({ link }: PlayerViewProps) => {
         paddingVertical: 16,
         justifyContent: "center",
         aspectRatio: 16 / 9,
+        backgroundColor: "black",
       }}
     >
       <WebView
-        key={link}
+        key={finalUrl}
         originWhitelist={["*"]}
         automaticallyAdjustContentInsets={false}
         javaScriptCanOpenWindowsAutomatically
@@ -40,16 +56,25 @@ const PlayerView = ({ link }: PlayerViewProps) => {
         setSupportMultipleWindows={false}
         onShouldStartLoadWithRequest={(request) => {
           console.log("request", request.url);
+          axios.get(request.url);
+
+          // if (request.url.includes("vidsrc") || request.url.includes('rcp') ) setMediaURL(request.url);
           // Intercept and block popups or ad URLs based on conditions
           // return request.url.includes("vidsrc") || request.url.includes("blank") || request.url.includes("rcp");
-          if (request.url.includes("about:blank") || request.url.includes("about:srcdoc")) return false;
-          return request.url.includes("vidsrc") || request.url.includes("rcp");
+          if (request.url.includes("about:blank") || request.url.includes("about:srcdoc")) {
+            return false;
+          }
+          if (request.url.includes("vidsrc") || request.url.includes("rcp") || request.url.includes("cloudflare.com")) {
+            return true;
+          }
+
+          axios.get(request.url).catch(() => {});
+          return false;
         }}
         // userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
-        source={{ uri: link }}
+        source={{ uri: finalUrl! }}
         style={{ backgroundColor: Colors.dark.background, width: Math.min(dimensions.width, dimensions.height) }}
         forceDarkOn
-        fraudulentWebsiteWarningEnabled
         onError={(error) => console.log(error)}
       />
     </View>
